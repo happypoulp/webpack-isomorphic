faad66a630b5f97174ea93258cc495f4c95e4c69
François Bonnefont <happypoulp@gmail.com>
2015-12-08 19:35:36 +0100

    Temporary(or not) ugly patch

--- webpack-isomorphic-tools.config.js
+++ webpack-isomorphic-tools.config.js
@@ -8,6 +8,32 @@ module.exports = {
     },
     svg: {
       extensions: ['svg'],
+      parser: function(module, options) {
+        console.log(module.source)
+        console.log(options)
+        console.log('##################################################')
+        console.log(global.CODEPTS)
+        console.log(global.STYLES)
+        console.log('####################### END ########################')
+        /*
+          PATCH node_modules/iconfont-loader/IconFontPlugin.js with:
+
+          stream.on("end", function () {
+              var module = modules.filter(function (module) {
+                  return module.rawRequest === "iconfont-loader";
+              })[0];
+
+              global.CODEPTS = plugin.codepoints
+              global.STYLES = plugin.styles
+
+              compilation.rebuildModule(module, callback);
+          });
+        */
+        var newSource = 'global.__FONT_ICON_PLUGIN_CODEPOINTS__=global.CODEPTS;' +
+          'global.__FONT_ICON_PLUGIN_STYLES__=global.STYLES;' +
+          module.source
+        return newSource
+      }
     },
     scss: {
       extensions: ['scss'],
