fa81f5d771f923d055f5200f8a7434acb4daa573
François Bonnefont <happypoulp@gmail.com>
2015-12-28 12:08:43 +0100

    Add "piping" support for server hot reload

--- bin/server.js
+++ bin/server.js
@@ -3,18 +3,38 @@ var path = require('path')
 global.__DEV__ = process.env.NODE_ENV
 global.__ISO_TOOLS__ = process.env.ISO_TOOLS
 
-var projectBasePath = path.resolve(__dirname, '..')
+var doRunServer = true
 
-var WebpackIsomorphicTools = require('webpack-isomorphic-tools')
+if (__DEV__) {
+  console.log('******* been here')
+  if (!require('piping')({
+      hook: true,
+      // ignore: /(\/\.|~$|\.json|\.scss$)/i
+    })) {
+    console.log('******* done that')
+    doRunServer = false
+  }
+}
 
-global.ISOMORPHIC_TOOLS = new WebpackIsomorphicTools(require('../webpack-isomorphic-tools.config.js'))
+if (doRunServer) {
+  console.log('******* RUN!!')
+  runServer()
+}
 
-ISOMORPHIC_TOOLS.development(__DEV__)
-  .server(projectBasePath, function() {
-    if (__DEV__) {
-      require('../fake-server.js')
-    }
-    else {
-      console.log('PROD SERVER NOT IMPLEMENTED')
-    }
-  })
+function runServer() {
+  var projectBasePath = path.resolve(__dirname, '..')
+
+  var WebpackIsomorphicTools = require('webpack-isomorphic-tools')
+
+  global.ISOMORPHIC_TOOLS = new WebpackIsomorphicTools(require('../webpack-isomorphic-tools.config.js'))
+
+  ISOMORPHIC_TOOLS.development(__DEV__)
+    .server(projectBasePath, function() {
+      if (__DEV__) {
+        require('../fake-server.js')
+      }
+      else {
+        console.log('PROD SERVER NOT IMPLEMENTED')
+      }
+    })
+}
